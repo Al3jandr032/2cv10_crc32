c8bea2309f5f283bbcedfa215583928921708e19
furby32 <pedrolop15@hotmail.com>
2014-03-14 17:58:29 -0600

    prueba

--- main.c
+++ main.c
@@ -2,25 +2,26 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/* 		1	0	0		1	1	0	0		0	0	0	1		0	0	0	1		1	1	0	1		1	0	1	1		0	1	1	1	*/
+/*				4					C					1						1				D				B					7		*/
 
 int main(int argc, char *argv[]) {
-	unsigned char pol[4]={0x04,0xC1,0x1D,0xB7};
-	unsigned char *msg="hola";
+	/*
+	K=4;
+	*/
+	unsigned char pol[8]={0x04,0x0C,0x01,0x01,0x01,0x0D,0x0B,0x07};
+	unsigned char msg[4]="hola";
 	unsigned char res;
 	
 	
-	res= 0x05^0x0b;
-	printf("%.1X \n",res);
-	
-	if (msg[0] & 0x08){
-		printf("%.1X \n",msg[0]);
-		printf("entro");
-	}
-	else{
-		printf("%.1X \n",msg[0]);
-		printf("no entro");
-	}
+	//res= 0x05^0x0b;
+	//printf("%.1X \n",res);
+	printf("%s \n",msg);
+	msg[0]<<= 1;
+	printf("%c",msg[0]);
 	/*
+	if (msg[0] & 0x08)
+	
 	0101
 	1010
 	1111
